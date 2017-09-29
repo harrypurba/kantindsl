d4c788b06c0749d877567a56b441fc9a04ba687d
Verisky Mega Jaya <13514018@std.stei.itb.ac.id>
2017-09-29 10:39:18 +0700

    add constructor

--- domainkantin.groovy
+++ domainkantin.groovy
@@ -1,28 +1,50 @@
 class Domainkantin{
 	static void main(String[] args){
-		println("Domain Kantin");
+		println("Domain Kantin")
 		
+		def listPelayan = [new Pelayan("Verisky"), new Pelayan("Rio")]
+		def listPelanggan = [new Pelanggan("Rio")]
+		def listMenu = [new Menu("Soto")]
+		listPelanggan[0].order(listMenu[0], 1, listPelayan[0]);
 	}
 }
 
 class Pelanggan{
-	String nama;
-
+	String nama
+	Pelanggan(_nama) {
+		nama = _nama
+	}
+	def order(menu, jumlah, pelayan) {
+		println("Pesan " + jumlah + " " + menu.nama + " kepada "  + pelayan.nama)
+	}
 }
 
 class Jurumasak{
-	String nama;
+	String nama
+	Jurumasak(_nama) {
+		nama = _nama
+	}
 }
 
 class Pelayan{
-	String nama;
+	String nama
+	Pelayan(_nama) {
+		nama = _nama
+	}
 }
 
 class Kasir{
-	String nama;
+	String nama
+	Kasir(_nama) {
+		nama = _nama
+	}
 }
 
 class Menu{
-	String nama;
+	String nama
+	int stok
+	Menu(_nama) {
+		nama = _nama
+	}
 }
 
